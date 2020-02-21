b5f909a94d4a26dfba0adb377f7f62b5edb2130f
theSYNcoder <papaigd@gmail.com>
2020-02-21 18:03:40 +0530

    Updates

---
+++ precache-manifest.3d91e276e2de89b3de7f72c155066589.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354ec45691e682c1c8d7617fd276b764",
+    "url": "/micro/index.html"
+  },
+  {
+    "revision": "31b3e76dc3c579e6265c",
+    "url": "/micro/static/css/main.2ddb9f06.chunk.css"
+  },
+  {
+    "revision": "931a830ab351778ae676",
+    "url": "/micro/static/js/2.21e466f8.chunk.js"
+  },
+  {
+    "revision": "b7ee6e442ef8f016d50ad4f9cb84c08a",
+    "url": "/micro/static/js/2.21e466f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b3e76dc3c579e6265c",
+    "url": "/micro/static/js/main.91127990.chunk.js"
+  },
+  {
+    "revision": "ec9b07cf7c08a1d64460",
+    "url": "/micro/static/js/runtime-main.03cedf4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/micro/static/media/logo.5d5d9eef.svg"
+  }
+]);
