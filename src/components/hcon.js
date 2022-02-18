8c1e49c0b9210ad5cb9527b2224a4c61952565c8
Shuvayan Ghosh Dastidar <papaigd@gmail.com>
2022-02-18 16:28:59 +0530

    fix vulnerabilities

--- src/components/hcon.js
+++ src/components/hcon.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import "./header.css";
-import Particles from 'react-particles-js';
+import Particles from "react-tsparticles";
 import GitHubButton from 'react-github-btn'
 
 
@@ -19,27 +19,87 @@ class Hcon extends React.Component{
         return (
           <div>
             <Particles
-              params={{
-                particles: {
-                  number: {
-                    value: 100
+              id="tsparticles"
+              init={() => {}}
+              loaded={() => {}}
+              options={{
+                background: {
+                  color: {
+                    value: "#fcc2cf",
                   },
-                  size: {
-                    value: 5
-                  }
                 },
+                fpsLimit: 120,
                 interactivity: {
                   events: {
-                    onhover: {
+                    onClick: {
                       enable: true,
-                      mode: "repulse"
-                    }
-                  }
-                }
+                      mode: "push",
+                    },
+                    onHover: {
+                      enable: true,
+                      mode: "repulse",
+                    },
+                    resize: true,
+                  },
+                  modes: {
+                    bubble: {
+                      distance: 400,
+                      duration: 2,
+                      opacity: 0.8,
+                      size: 40,
+                    },
+                    push: {
+                      quantity: 4,
+                    },
+                    repulse: {
+                      distance: 200,
+                      duration: 0.4,
+                    },
+                  },
+                },
+                particles: {
+                  color: {
+                    value: "#ffffff",
+                  },
+                  links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                  },
+                  collisions: {
+                    enable: true,
+                  },
+                  move: {
+                    direction: "none",
+                    enable: true,
+                    outMode: "bounce",
+                    random: false,
+                    speed: 6,
+                    straight: false,
+                  },
+                  number: {
+                    density: {
+                      enable: true,
+                      area: 800,
+                    },
+                    value: 80,
+                  },
+                  opacity: {
+                    value: 0.5,
+                  },
+                  shape: {
+                    type: "circle",
+                  },
+                  size: {
+                    random: true,
+                    value: 5,
+                  },
+                },
+                detectRetina: true,
               }}
-              style={{
-                backgroundColor: "#fcc2cf"
-              }}
+              
             />
             <Link to="/">
               <div className="content">Micro</div>
